a26460a69cbab75e5bd3e116d463c3ec70630aaa
xXfokumeXx <radek.cerny847@gmail.com>
2025-07-11 13:03:05 +0200

    added list comprehension of formatted dates

--- app.py
+++ app.py
@@ -11,4 +11,14 @@ def home():
         entry_content = request.form.get("content")
         formatted_date = datetime.datetime.today().strftime("%Y-%m-%d")
         entries.append((entry_content, formatted_date))
-    return render_template("add.html", entries=entries)
+
+    entries_formatted = [
+        (
+           entry[0],
+           entry[1],
+           datetime.datetime.strptime(entry[1], "%Y-%m-%d")
+        )
+        for entry in entries
+    ]
+
+    return render_template("add.html", entries=entries_formatted)

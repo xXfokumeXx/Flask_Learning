06a226a4fe661c4f71f635eb7171c2b7993e87ab
xXfokumeXx <radek.cerny847@gmail.com>
2025-07-11 11:45:12 +0200

    entries saved into list, for simple running an ap as an examplu. instead of using DBS.

--- app.py
+++ app.py
@@ -3,10 +3,12 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+entries = []
+
 @app.route("/", methods=["GET", "POST"])
 def home():
     if request.method == "POST":
         entry_content = request.form.get("content")
         formatted_date = datetime.datetime.today().strftime("%Y-%m-%d")
-        print(entry_content, formatted_date)
-    return render_template("add.html")
+        entries.append((entry_content, formatted_date))
+    return render_template("add.html", entries=entries)

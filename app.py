7744c76604933e3d290bc288e299367d131f7683
xXfokumeXx <radek.cerny847@gmail.com>
2025-07-13 04:34:37 +0200

    working inserting data to my mongoDB

--- app.py
+++ app.py
@@ -3,18 +3,19 @@ from flask import Flask, render_template, request
 from pymongo import MongoClient
 
 app = Flask(__name__)
-client = MongoClient("mongodb+srv://programkogjj:programovanijebest!?@cluster0.sryitvq.mongodb.net/")
+client = MongoClient("mongodb+srv://programkogjj:programovanijebest%21%3F@cluster0.sryitvq.mongodb.net/")
 app.db = client.mynotes
 
 entries = []
 
 @app.route("/", methods=["GET", "POST"])
 def home():
-    print([entry for entry in app.db.entries.find({})])
+    #print([entry for entry in app.db.entries.find({})])
     if request.method == "POST":
         entry_content = request.form.get("content")
         formatted_date = datetime.datetime.today().strftime("%Y-%m-%d")
         entries.append((entry_content, formatted_date))
+        app.db.entries.insert_one({"content": entry_content, "date": formatted_date})
 
     entries_formatted = [
         (

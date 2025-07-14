f2ce14a3cdd7b32fcb7e42bcc29c75e68bcbf5db
xXfokumeXx <radek.cerny847@gmail.com>
2025-07-14 21:59:06 +0200

    created .flaskenv and isntalled python-dotenv, updated app.py

--- My_Notes_app/app.py
+++ My_Notes_app/app.py
@@ -2,25 +2,27 @@ import datetime
 from flask import Flask, render_template, request
 from pymongo import MongoClient
 
-app = Flask(__name__)
-client = MongoClient("mongodb+srv://programkogjj:programovanijebest%21%3F@cluster0.sryitvq.mongodb.net/")
-app.db = client.mynotes
+def create_app():
+    app = Flask(__name__)
+    client = MongoClient("mongodb+srv://programkogjj:programovanijebest%21%3F@cluster0.sryitvq.mongodb.net/")
+    app.db = client.mynotes
 
-@app.route("/", methods=["GET", "POST"])
-def home():
-    #print([entry for entry in app.db.entries.find({})])
-    if request.method == "POST":
-        entry_content = request.form.get("content")
-        formatted_date = datetime.datetime.today().strftime("%Y-%m-%d")
-        app.db.entries.insert_one({"content": entry_content, "date": formatted_date})
+    @app.route("/", methods=["GET", "POST"])
+    def home():
+        #print([entry for entry in app.db.entries.find({})])
+        if request.method == "POST":
+            entry_content = request.form.get("content")
+            formatted_date = datetime.datetime.today().strftime("%Y-%m-%d")
+            app.db.entries.insert_one({"content": entry_content, "date": formatted_date})
 
-    entries_formatted = [
-        (
-           entry["content"],
-           entry["date"],
-           datetime.datetime.strptime(entry["date"], "%Y-%m-%d").strftime("%b %d")
-        )
-        for entry in app.db.entries.find({})
-    ]
+        entries_formatted = [
+            (
+            entry["content"],
+            entry["date"],
+            datetime.datetime.strptime(entry["date"], "%Y-%m-%d").strftime("%b %d")
+            )
+            for entry in app.db.entries.find({})
+        ]
 
-    return render_template("add.html", entries=entries_formatted)
+        return render_template("add.html", entries=entries_formatted)
+    return app
